6040d1b6f57af9fac27edf93786b3521b5d19a8d
Kevin Benton <ktbenton@indiana.edu>
2013-09-25 18:09:40 -0400

    more generalizable

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ ytube_comment_formatter
 =======================
 
 Script to help the struggling youtube commenter format comments into youtube-comment-compatible strings
+
+
+Usage
+-----
+Send a colon-separated string via standard input into the script:
+
+```
+echo "North Korea:Doesn't even lift" | ./textstack.py
+```
+
+Alternatively, it can read colon-separated strings from a file:
+
+```
+textstack.py strings.txt
+```
--- textstack.py
+++ textstack.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
+import fileinput
 
-strings = ['North Korea', "Doesn't even lift"]
 output = ''
-for i,s in enumerate(strings):
-    if (i % 2) == 0:
-        l = len(s)
-        while l>0:
-            output = output + s[0:l] + "\n"
-            l = l - 1
-    else:
-        l = 1
-        while l<=len(s):
-            output = output + s[0:l] + "\n"
-            l = l + 1
+lines = [line for line in fileinput.input()]
+
+for line in lines:
+    strings = line.split(':')
+    for i,s in enumerate(strings):
+        if (i % 2) == 0:
+            l = len(s)
+            while l>0:
+                output = output + s[0:l] + "\n"
+                l = l - 1
+        else:
+            l = 1
+            while l<len(s):
+                output = output + s[0:l] + "\n"
+                l = l + 1
 
 print output
